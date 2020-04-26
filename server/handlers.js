b7d4cf6798cdcbdfdbc5325d638ea9672ec03212
Matteo Mortelliti <mortellitimatteo@gmail.com>
2020-04-26 17:01:06 -0400

    genere id started - w some styling

--- server/handlers.js
+++ server/handlers.js
@@ -36,10 +36,14 @@ const handleGenreId = async (req, res) => {
     // // do something
 
 
+    console.log(req.params)
+
+    const { genreId } = req.params;
+    console.log(req.params)
 
     var options = {
         method: 'GET',
-        url: `https://api.themoviedb.org/3/discover/movie?with_genres=18&sort_by=vote_average.desc&vote_count.gte=10`,
+        url: `https://api.themoviedb.org/3/discover/movie?with_genres=${genreId}&original_language=en&sort_by=popularity.desc&vote_count.gte=10`,
         headers: {
             authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5ZDA5Nzc1ZGIxMGQwMzg3ZGY5YWEwNDYzNjZiNzE3MiIsInN1YiI6IjVlYTFlODY5YWY0MzI0MDAxZDllN2Q0MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.OxkyvbtGbap8tCc1NN3pATUNlPSNqhOGKcWk8uCvOSc'
         }
